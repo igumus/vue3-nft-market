660688f7892750ea1ae5e6a496773929aa9917d5
igumus <ibrhm.gumus@gmail.com>
2022-04-23 01:11:48 +0300

    configured: hardhat dependency

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -5,12 +5,9 @@ async function main() {
   const greeter = await Greeter.deploy("Hello, Hardhat!");
 
   await greeter.deployed();
-
   console.log("Greeter deployed to:", greeter.address);
 }
 
-// We recommend this pattern to be able to use async/await everywhere
-// and properly handle errors.
 main()
   .then(() => process.exit(0))
   .catch((error) => {
