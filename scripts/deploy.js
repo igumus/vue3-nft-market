8338009182f89abae7df6642ad0939f37c143f82
igumus <ibrhm.gumus@gmail.com>
2022-04-23 03:01:12 +0300

    added: NFT,NFTMarket solidity files, updated deployment

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,11 +1,26 @@
 const hre = require("hardhat");
+const fs = require("fs");
 
 async function main() {
-  const Greeter = await hre.ethers.getContractFactory("Greeter");
-  const greeter = await Greeter.deploy("Hello, Hardhat!");
+  const NFTMarket = await hre.ethers.getContractFactory("NFTMarket");
+  const nftMarket = await NFTMarket.deploy();
+  await nftMarket.deployed();
+  console.log("NFTMarket deployed to:", nftMarket.address);
 
-  await greeter.deployed();
-  console.log("Greeter deployed to:", greeter.address);
+
+  const NFT = await hre.ethers.getContractFactory("NFT");
+  const nft = await NFT.deploy(nftMarket.address);
+  await nft.deployed();
+  console.log("NFT deployed to:", nft.address);
+
+ 
+  let config = `
+  export const nftmarketaddress = "${nftMarket.address}"
+  export const nftaddress = "${nft.address}"
+  `
+
+  let data = JSON.stringify(config)
+  fs.writeFileSync('config.js', JSON.parse(data)) 
 }
 
 main()
