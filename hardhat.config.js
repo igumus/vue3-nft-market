16a81b85d08f2c78b1fdc252d4eb6de6c9ceb88f
igumus <ibrhm.gumus@gmail.com>
2022-04-28 22:47:07 +0300

    added: ganache configuration in hardhat config

--- hardhat.config.js
+++ hardhat.config.js
@@ -7,10 +7,13 @@ module.exports = {
   solidity: {
     version: "0.8.4",
   },
-  defaultNetwork: "hardhat",
   networks: {
-    hardhat: {
-      chainId: 1337
-    },
+    ganache: {
+      url: "http://127.0.0.1:7545",
+      chainId: 1337,
+      accounts: [
+        `0x9e58e0265e9910670e7754eb66495433cf69420b09fb317de088a530c2eea7c4`,
+      ],
+    }
   }
 };
