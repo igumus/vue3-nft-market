ef92d60578f3c6cfa9b007575a7dab368125294f
igumus <ibrhm.gumus@gmail.com>
2022-05-01 03:04:16 +0300

    updated: empty market error in view

--- src/views/ExploreView.vue
+++ src/views/ExploreView.vue
@@ -1,8 +1,11 @@
 <template>
   <div class="flex justify-center">
    <div class="px-4 max-w-7xl">
-        <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4 pt-4">
+        <div v-if="items" class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4 pt-4">
           <NFTCard v-for="(nft, idx) in items" :key="idx" :item="nft"/>
+        </div>
+        <div v-else>
+          <h1 className="py-10 px-20 text-3xl">No NFTs in market</h1>
         </div>
       </div>
   </div>
