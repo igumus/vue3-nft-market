6a5c3cc7fd06120ba9c7779a62b38408e96f7443
igumus <ibrhm.gumus@gmail.com>
2022-05-01 04:45:54 +0300

    added: shortcut actions in views when no items found

--- src/views/CreatedAssetListView.vue
+++ src/views/CreatedAssetListView.vue
@@ -5,8 +5,15 @@
         <NFTCard v-for="(nft, idx) in items" :key="idx" :item="nft"/>
       </div>
     </div>
-    <div v-else>
-      <h1 className="py-10 px-20 text-3xl">No NFTs Created</h1>
+   <div v-else class="max-w-7xl mx-auto py-12 px-4 sm:px-6 lg:py-16 lg:px-8 lg:flex lg:items-center lg:justify-between">
+      <h2 class="text-3xl font-extrabold tracking-tight text-gray-900 sm:text-4xl mr-4">
+        <span class="block">Market has no items, Ready to create one ?</span>
+      </h2>
+      <div class="mt-8 flex lg:mt-0 lg:flex-shrink-0">
+        <div class="ml-3 inline-flex rounded-md shadow">
+          <router-link to="/createAsset" class="inline-flex items-center justify-center px-5 py-3 border border-transparent text-base font-medium rounded-md text-indigo-600 bg-white hover:bg-indigo-50">Create NFT</router-link>
+        </div>
+      </div>
     </div>
   </div>
 </template>
--- src/views/ExploreView.vue
+++ src/views/ExploreView.vue
@@ -4,8 +4,15 @@
         <div v-if="items.length" class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4 pt-4">
           <NFTCard v-for="(nft, idx) in items" :key="idx" :item="nft"/>
         </div>
-        <div v-else>
-          <h1 className="py-10 px-20 text-3xl">No NFTs in market</h1>
+        <div v-else class="max-w-7xl mx-auto py-12 px-4 sm:px-6 lg:py-16 lg:px-8 lg:flex lg:items-center lg:justify-between">
+              <h2 class="text-3xl font-extrabold tracking-tight text-gray-900 sm:text-4xl mr-4">
+                <span class="block">Market has no items, Ready to create one ?</span>
+              </h2>
+              <div class="mt-8 flex lg:mt-0 lg:flex-shrink-0">
+                <div class="ml-3 inline-flex rounded-md shadow">
+                  <router-link to="/createAsset" class="inline-flex items-center justify-center px-5 py-3 border border-transparent text-base font-medium rounded-md text-indigo-600 bg-white hover:bg-indigo-50">Create NFT</router-link>
+                </div>
+              </div>
         </div>
       </div>
   </div>
